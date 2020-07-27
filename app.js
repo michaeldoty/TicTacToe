428eb45316a93ca04b5dc464df4fa2b3aa93935c
Michael Doty <michaeldoty@outlook.com>
2020-07-27 15:45:54 -0700

    complete game

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-
-let playerTurn = 'X';
-
-let cell0 = {
-  value: 'X',
-  hasBeenClicked: false
+window.onload = function() {
+  document.getElementById('scoreX').innerHTML = `X: ${xWins}`;
+  document.getElementById('scoreO').innerHTML = `O: ${oWins}`;
 }
 
+let isWinner = false;
+let playerTurn = 'X';
+let oWins = 0;
+let xWins = 0;
+
+let cell0 = {
+  value: 'empty',
+  hasBeenClicked: false
+}
 let cell1 = {
-  value: 'X',
+  value: 'empty',
   hasBeenClicked: false
 }
 let cell2 = {
-  value: 'X',
+  value: 'empty',
   hasBeenClicked: false
 }
 let cell3 = {
-  value: 'X',
+  value: 'empty',
   hasBeenClicked: false
 }
 let cell4 = {
-  value: 'X',
+  value: 'empty',
   hasBeenClicked: false
 }
 let cell5 = {
-  value: 'X',
+  value: 'empty',
+  hasBeenClicked: false
+}
+let cell6 = {
+  value: 'empty',
+  hasBeenClicked: false
+}
+let cell7 = {
+  value: 'empty',
+  hasBeenClicked: false
+}
+let cell8 = {
+  value: 'empty',
   hasBeenClicked: false
 }
 
-
 function toggleValue(cellValue) {
-  //if player turn is null (player X turn);
   console.log(cellValue);
 
   if(cellValue === 'cell0') {
     if(cell0.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell0').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell0').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell0.value = 'X'
       document.getElementById('cell0').innerHTML = cell0.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell0').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell0').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell0.value = 'O'
@@ -49,7 +65,7 @@ function toggleValue(cellValue) {
       }
 
     cell0.hasBeenClicked = true;
-    console.log(cell0.hasBeenClicked);
+
     console.log('player turn', playerTurn);
   }
 
@@ -57,12 +73,12 @@ function toggleValue(cellValue) {
     if(cell1.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell1').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell1').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell1.value = 'X'
       document.getElementById('cell1').innerHTML = cell1.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell1').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell1').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell1.value = 'O'
@@ -78,12 +94,12 @@ function toggleValue(cellValue) {
     if(cell2.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell2').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell2').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell2.value = 'X'
       document.getElementById('cell2').innerHTML = cell2.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell2').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell2').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell2.value = 'O'
@@ -99,12 +115,12 @@ function toggleValue(cellValue) {
     if(cell3.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell3').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell3').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell3.value = 'X'
       document.getElementById('cell3').innerHTML = cell3.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell3').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell3').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell3.value = 'O'
@@ -120,12 +136,12 @@ function toggleValue(cellValue) {
     if(cell4.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell4').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell4').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell4.value = 'X'
       document.getElementById('cell4').innerHTML = cell4.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell4').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell4').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell4.value = 'O'
@@ -141,12 +157,12 @@ function toggleValue(cellValue) {
     if(cell5.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell5').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell5').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell5.value = 'X'
       document.getElementById('cell5').innerHTML = cell5.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell5').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell5').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell5.value = 'O'
@@ -162,12 +178,12 @@ function toggleValue(cellValue) {
     if(cell6.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell6').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell6').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell6.value = 'X'
       document.getElementById('cell6').innerHTML = cell6.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell6').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell6').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell6.value = 'O'
@@ -183,12 +199,12 @@ function toggleValue(cellValue) {
     if(cell7.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell7').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell7').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell7.value = 'X'
       document.getElementById('cell7').innerHTML = cell7.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell7').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell7').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell7.value = 'O'
@@ -204,12 +220,12 @@ function toggleValue(cellValue) {
     if(cell8.hasBeenClicked === true) {
       alert('obviously you cant play there...');
     }
-    if(playerTurn === 'X' && document.getElementById('cell8').innerHTML === "") {
+    if(playerTurn === 'X' && document.getElementById('cell8').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'O'
       cell8.value = 'X'
       document.getElementById('cell8').innerHTML = cell8.value;
-    } else if (playerTurn === 'O' && document.getElementById('cell8').innerHTML === "") {
+    } else if (playerTurn === 'O' && document.getElementById('cell8').innerHTML === "" && isWinner === false) {
       console.log('click');
       playerTurn = 'X'
       cell8.value = 'O'
@@ -221,10 +237,115 @@ function toggleValue(cellValue) {
     console.log('player turn', playerTurn);
   }
 
+  checkWinner();
+}
+
+let checkWinner = () => {
+  if(isWinner === true) {
+    winner = '';
+      if(playerTurn === 'X') {
+        winner = 'O';
+      } else {
+        winner = 'X';
+      }
+      alert(`${winner} has already won!  Reset the game!`)
+    }
+
+  if(cell0.value === cell1.value && cell0.value === cell2.value && cell0.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell3.value === cell4.value && cell3.value === cell5.value && cell3.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell6.value === cell7.value && cell6.value === cell8.value && cell6.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell0.value === cell3.value && cell0.value === cell6.value && cell0.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell1.value === cell4.value && cell1.value === cell7.value && cell1.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell2.value === cell5.value && cell2.value === cell8.value && cell2.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell0.value === cell4.value && cell0.value === cell8.value && cell0.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if(cell2.value === cell4.value && cell2.value === cell6.value && cell2.value !== 'empty') {
+    isWinner = true;
+  } else {
+    console.log('no winner yet');
+  }
+
+  if (isWinner === true) {
+    winner = '';
+    if(playerTurn === 'X') {
+      winner = 'O';
+    } else {
+      winner = 'X';
+    }
+    if(winner === 'X') {
+      console.log('winner', winner)
+      xWins = xWins + 1;
+      document.getElementById('scoreX').innerHTML = `X: ${xWins}`;
+    } else {
+      oWins = oWins + 1;
+      document.getElementById('scoreO').innerHTML = `O: ${oWins}`;
+    }
+    setTimeout(function() {alert(`${winner} WINS!`);}, 100);
+  }
 }
 
 
-
-
-
-
+let reset = () => {
+  document.getElementById('cell0').innerHTML = '';
+  cell0.value = 'empty';
+  cell0.hasBeenClicked = false;
+  document.getElementById('cell1').innerHTML = '';
+  cell1.value = 'empty';
+  cell1.hasBeenClicked = false;
+  document.getElementById('cell2').innerHTML = '';
+  cell2.value = 'empty';
+  cell2.hasBeenClicked = false;
+  document.getElementById('cell3').innerHTML = '';
+  cell3.value = 'empty';
+  cell3.hasBeenClicked = false;
+  document.getElementById('cell4').innerHTML = '';
+  cell4.value = 'empty';
+  cell4.hasBeenClicked = false;
+  document.getElementById('cell5').innerHTML = '';
+  cell5.value = 'empty';
+  cell5.hasBeenClicked = false;
+  document.getElementById('cell6').innerHTML = '';
+  cell6.value = 'empty';
+  cell6.hasBeenClicked = false;
+  document.getElementById('cell7').innerHTML = '';
+  cell7.value = 'empty';
+  cell7.hasBeenClicked = false;
+  document.getElementById('cell8').innerHTML = '';
+  cell8.value = 'empty';
+  cell8.hasBeenClicked = false;
+  isWinner = false;
+}
