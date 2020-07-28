23e9308a7c6f2a18a4d257ae38bba66c56a4057b
Michael Doty <michaeldoty@outlook.com>
2020-07-27 17:24:57 -0700

    Assignment Complete

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 window.onload = function() {
   document.getElementById('scoreX').innerHTML = `X: ${xWins}`;
   document.getElementById('scoreO').innerHTML = `O: ${oWins}`;
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
+
 
 let isWinner = false;
 let playerTurn = 'X';
@@ -349,3 +377,5 @@ let reset = () => {
   cell8.hasBeenClicked = false;
   isWinner = false;
 }
+
+
